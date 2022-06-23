0255e7082961fcd782819598f66db6df2e65f6e4
jung6888 <107675987+jung6888@users.noreply.github.com>
2022-06-23 18:18:49 +0700

    Add files via upload
    new changes with minimal css

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- CSS only -->
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+	</head>
+<body>
+
+
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid " style="background-color: #c4f4df">
+                <div class="row my-auto" style="width:150vw; height: 100%" >
+                    <div class="col-12 col-xs-12 col-sm-12 col-lg-4 col-xl-3 my-auto justify-content-end" >
+                    <button type="button" id="menu-toggle" class="btn  mt-1 mb-3" style="color: #152238">
+                        <i class="fas fa-align-left text-success "></i>
+                        
+                    </button>
+                    <span class="text-dark" style="font-size: 2rem">Dashboard</span>  
+                    </div>      
+                    <div class="d-none d-lg-block d-xl-block col-lg-8 col-xl-9 my-auto" >
+                        <div class="collapse navbar-collapse justify-content-end" >
+                            <ul class="navbar-nav ">
+                                <li class="nav-item text-dark">
+                                   <i class= "fa fa-user fa-lg">
+                                   <span> Welcome <?php 
+         
+                                   $msgg = $_SESSION["username"];
+                                   echo $msgg;
+                                   ?></span>
+                                   </i>
+                                </li>
+                            </ul>
+                    </div>
+            </div>
+            </div>
+        </nav>
+
+        <script src="script.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+//unset($_POST["username"]);
+//unset($_POST["password"]);
 $msg="";
 if(isset($_POST["button_login"])){
 	$username =  $_POST["username"];
@@ -22,24 +24,54 @@ if(isset($_POST["button_login"])){
 <html>
 	<head>
 	<meta charset="UTF-8" />
-	<link rel="stylesheet" href="index.css" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    <meta charset="utf-8">
 	</head>
-	<body>
-	<div class="form-center">
-		<h1 class="text-center" id="header1">Login</h1>
-		<h3 class="text-center"><?php echo $msg;?></h3>
-		<form class="text-center" id="login-form"action='<?php echo $_SERVER["PHP_SELF"]?>' method="post">
-		<div >
-			Username : <input type="text" name="username" /><br />
-		</div>
-			Password : <input  type="password" name="password"><br />
-			<input  type="submit" value="Login" name="button_login" />
-		</form>
-	</div>	
+<body> 
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-5 mx-auto mt-5 pt-5">
+                <div class="card">
+                        <h1 class="card-title mb-1 mt-1 text-center">Login</h1>
+                        <hr>
+                        <h3 class="text-center"><?php echo $msg;?></h3>
+                        <form class="text-center" id="login-form"action='<?php echo $_SERVER["PHP_SELF"]?>' method="post">
+          
+                            <div class="form-group mx-3">
+                                <div class="input-group">
+                                    <div class="input-group-prepend h-100">
+                                        <span class="input-group-text"><i class="fa fa-user fa-2x"></i> </span>
+                                    </div>
+                                    <input name="username" class="form-control" placeholder="Enter your username" type="text" required>
+                                </div>  
+                            </div>
+                    
+                            <br>
+                            <div class="form-group mx-3">
+	                                <div class="input-group">
+		                                <div class="input-group-prepend">
+		                                    <span class="input-group-text"> <i class="fa fa-lock fa-2x"></i> </span>
+		                                </div>
+	                                    <input name="password" class="form-control" placeholder="******" type="password">
+	                                </div> <!-- input-group.// -->
+	                        </div> <!-- form-group// -->
+                            <br>
+	                        <div class="form-group text-center">
+
+                            <input class="btn btn-success btn-lg" type="submit" value="Login" name="button_login" />
+	                        </div> <!-- form-group// -->
+                           <br>
+                        </form>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+       
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 	</body>
 </html>
