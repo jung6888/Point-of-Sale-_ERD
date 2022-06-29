a76b1d079ef1f99b4e5244288c937b397d865521
Song Pitou <68115235+Pitou911@users.noreply.github.com>
2022-06-29 10:38:02 +0700

    Update food.php

---
+++ food.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- <link rel="stylesheet" href="style.css" /> -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+      crossorigin="anonymous"
+    />
+  </head>
+    <body>
+        <div class="title p-3"><h1>Foods & Drinks</h1></div>
+        <div class="container-fluid d-flex justify-content-center">
+            <div class="d-flex justify-content-center flex-wrap">
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/drinks/coca_cola.jpg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Coca Cola</h5>
+                            <p class="card-text">$2.49</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/drinks/copper_cocktails.jpeg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Copper Cocktail</h5>
+                            <p class="card-text">$4.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/drinks/lemon_melon.jpeg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Lemon Melon</h5>
+                            <p class="card-text">$4.49</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/drinks/lemon_whisky.jpeg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Lemon Whiskey</h5>
+                            <p class="card-text">$6.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/drinks/lemonate.jpeg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Lemonate</h5>
+                            <p class="card-text">$5.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/drinks/love_portion.jpg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Love Portion</h5>
+                            <p class="card-text">$4.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/drinks/lychee_cocktail.jpeg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Lychee Cocktail</h5>
+                            <p class="card-text">$6.49</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/drinks/mixed_berries_smoothies.jpeg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Mixed Berries Smoothies</h5>
+                            <p class="card-text">$6.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/drinks/rum_cocktail.jpg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Rum Cocktail</h5>
+                            <p class="card-text">$3.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/drinks/yogurt_smoothies.jpeg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Yogurt Smoothies</h5>
+                            <p class="card-text">$5.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/foods/dona_dessert.jpg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Dona Dessert</h5>
+                            <p class="card-text">$3.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/foods/honey_bread_with_berry.jpg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Honey Bread Berry</h5>
+                            <p class="card-text">$4.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/foods/indian_fried_chicken.jpg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Indian Fried Chicken</h5>
+                            <p class="card-text">$5.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/foods/makota_dish.jpeg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Makota Dish</h5>
+                            <p class="card-text">$12.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/foods/minced_pork_pasta.jpeg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Minced Pork Pasta</h5>
+                            <p class="card-text">$6.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/foods/pasta_with_tomato_sauce.jpg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Pasta Tomato Sauce</h5>
+                            <p class="card-text">$4.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/foods/signature_lamp.jpeg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Signature Lamp</h5>
+                            <p class="card-text">$14.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/foods/special_rice.jpeg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Special Sushi</h5>
+                            <p class="card-text">$24.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/foods/viggie_pizza.jpg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Viggie Pizza</h5>
+                            <p class="card-text">$14.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/foods/whooper_burger.jpg" class="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Whooper Burger</h5>
+                            <p class="card-text">$6.99</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+  
+</html>
